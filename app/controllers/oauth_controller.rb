a4121ce105828db98ae593e60be68220a8ec8da6
Don Gorra <djgorra93@gmail.com>
2023-07-11 15:04:55 -0400

    more oauth changes

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -12,10 +12,10 @@ class OauthController < ApplicationController
     def callback
         code = params[:code]
         client = OAuth2::Client.new('6e5be73d7bb84defbfe49d9fb5eb4581', 'R2ulNak8YNKGV4UNoQ74yonVEq4zPMT1',
-                            site: 'https://us.api.blizzard.com',
+                            site: 'https://oauth.battle.net',
                             authorize_url: '/authorize',
                             token_url: '/token')
-        @access = client.auth_code.get_token(code, redirect_uri: 'https://wow-app-rails-5c78013cc11c.herokuapp.com/oauth2/callback')
+        @access = client.auth_code.get_token(code, redirect_uri: 'https://wow-app-rails-5c78013cc11c.herokuapp.com/oauth2/callback', scope: 'wow.profile', grant_type: 'authorization_code')
         # @response = @access.get('/profile/user/wow', params: {'region' => 'us', 'namespace' => 'profile-us', 'locale' => 'en_US'})
     end
 end
