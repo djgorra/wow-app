fdf6b108898c334b0cd2f687aa732af80b4e1f6f
Don Gorra <djgorra93@gmail.com>
2023-07-11 14:51:26 -0400

    oauth progress

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,13 +1,16 @@
+require 'oauth2'
+
 class OauthController < ApplicationController
     def login
         client = OAuth2::Client.new('6e5be73d7bb84defbfe49d9fb5eb4581', 'R2ulNak8YNKGV4UNoQ74yonVEq4zPMT1',
                             site: 'https://oauth.battle.net/',
                             authorize_url: '/authorize',
                             token_url: '/token')
-        client.auth_code.authorize_url(redirect_uri: 'https://wow-app-rails-5c78013cc11c.herokuapp.com/oauth2/callback')
-        redirect_to client.auth_code.authorize_url(redirect_uri: 'https://wow-app-rails-5c78013cc11c.herokuapp.com/oauth2/callback')
+        redirect_to client.auth_code.authorize_url(redirect_uri: 'https://wow-app-rails-5c78013cc11c.herokuapp.com/oauth2/callback', scope: 'wow.profile', state: '123')
     end
 
     def callback
+        code = params[:code]
+        @access = client.auth_code.get_token(code, redirect_uri: 'https://wow-app-rails-5c78013cc11c.herokuapp.com/oauth2/callback')
     end
 end
