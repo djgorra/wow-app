9bc50c18959e122dc24d61bea8bc4c81b7a9c36f
Don Gorra <djgorra93@gmail.com>
2023-07-19 15:24:19 -0400

    Users show method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
     before_action :authenticate_user!
     respond_to :json 
     def show
+        render :json=>current_user.as_json
     end
     
     def update
