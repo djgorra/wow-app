b38a8c0bcba587bed057ba7b18a249cb3cfc602e
Don Gorra <djgorra93@gmail.com>
2023-08-14 14:42:25 -0400

    Addad avatar method to characters

--- app/models/character.rb
+++ app/models/character.rb
@@ -14,9 +14,14 @@ class Character < ApplicationRecord
     enum :character_class => { "paladin"=>0, "warrior"=>1, "hunter"=>2, "rogue"=>3,
      "priest"=>4, "shaman"=>5, "mage"=>6, "warlock"=>7, "druid"=>8, "death_knight"=>9 }
 
+
+    def avatar 
+      "/races/race_#{race.gsub(" ", "").gsub("_", "")}_#{gender}.jpg"
+    end
+
     def as_json(options = {})
      out = {}
-      [:id, :name, :race, :gender, :user_id, :primary_spec_id, :secondary_spec_id, :character_class_id].each do |key|
+      [:id, :name, :race, :gender, :user_id, :primary_spec_id, :secondary_spec_id, :character_class_id, :avatar].each do |key|
        out[key] = self.send(key)
      end
      out
