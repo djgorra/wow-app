642223cb39ea7476d5166507b0ce9142d3c68848
Don Gorra <djgorra93@gmail.com>
2023-08-18 14:41:03 -0400

    spec icon changes

--- app/models/character.rb
+++ app/models/character.rb
@@ -24,11 +24,11 @@ class Character < ApplicationRecord
     end
 
     def primary_spec_icon
-      "/specs/#{character_class.name.capitalize.gsub(" ", "").gsub("_", "")}#{primary_spec.name.gsub(" ", "").gsub("_", "")}.jpg"
+      "/specs/#{primary_spec.name.gsub(" ", "").gsub("_", "")}#{character_class.name.capitalize.gsub(" ", "").gsub("_", "")}.jpg"
     end
 
     def secondary_spec_icon
-      "/specs/#{character_class.name.capitalize.gsub(" ", "").gsub("_", "")}#{secondary_spec.name.gsub(" ", "").gsub("_", "")}.jpg"
+      "/specs/#{secondary_spec.name.gsub(" ", "").gsub("_", "")}#{character_class.name.capitalize.gsub(" ", "").gsub("_", "")}.jpg"
     end
 
     def as_json(options = {})
