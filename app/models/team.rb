76a8ad5a8fd1c7a9034bf7f1be745c7f69701d7a
Don Gorra <djgorra93@gmail.com>
2023-10-04 15:58:41 -0400

    Update team.rb as_json method

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,12 @@ class Team < ApplicationRecord
     has_many :runs
     has_many :team_characters, :class_name => "TeamCharacter"
     has_many :characters, through: :team_characters
+
+    def as_json(options = {})
+        out = {}
+        [:id, :name, :user_id, :characters].each do |key|
+            out[key] = self.send(key)
+        end
+        out
+    end
 end
