6ee8640e0ee1a9c25b10e0da294c39bb965ea8dc
Don Gorra <djgorra93@gmail.com>
2023-10-25 14:51:26 -0400

    Added custom as_json to run.rb

--- app/models/run.rb
+++ app/models/run.rb
@@ -4,4 +4,15 @@ class Run < ApplicationRecord
     has_many :team_characters
     has_many :battles
     has_many :drops, through: :battles
+
+    def raid_name
+        raid.name
+    end
+
+    def as_json(options = {})
+        out = {}
+        [:id, :raid_id, :raid_name, :created_at, :team_characters, :battles].each do |key|
+            out[key] = self.send(key)
+        end
+    end
 end
