b4feee18eccdce78ef1158a8e6acea5b5b706e65
Don Gorra <djgorra93@gmail.com>
2023-11-27 12:58:40 -0500

    Added missing spell icon

--- app/admin/drops.rb
+++ app/admin/drops.rb
@@ -1,3 +1,23 @@
 ActiveAdmin.register Drop do
-
+    permit_params :character_battle_id, :item_id, :disenchanted
+    
+    index do
+        selectable_column
+        id_column
+        column :character_battle do |drop|
+            drop.character_battle.character.name
+        end
+        column :item
+        column :disenchanted
+        actions
+    end
+    
+    filter :disenchanted
+    
+    form do |f|
+        f.inputs do
+            f.input :disenchanted
+        end
+        f.actions
+    end
 end
