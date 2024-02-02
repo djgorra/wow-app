f379523e308a461560eaa48fbe0248d413d099e4
Don Gorra <djgorra93@gmail.com>
2024-02-02 13:33:14 -0500

    Image fix test

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,8 +1,12 @@
 class Version < ApplicationRecord
 
+    validates :version_name, presence: true
+    validates uniqueness: true
+
     def self.seed
         Version.create(version_name: "Vanilla")
         Version.create(version_name: "TBC")
         Version.create(version_name: "Wrath")
+        Version.create(version_name: "SoD")
     end
 end
