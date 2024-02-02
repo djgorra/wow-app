dd986a86f6a50baabd8592b4b28e0b53c5db2793
Don Gorra <djgorra93@gmail.com>
2024-02-02 13:43:09 -0500

    Revert "Image fix test"
    This reverts commit f379523e308a461560eaa48fbe0248d413d099e4.

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,12 +1,8 @@
 class Version < ApplicationRecord
 
-    validates :version_name, presence: true
-    validates uniqueness: true
-
     def self.seed
         Version.create(version_name: "Vanilla")
         Version.create(version_name: "TBC")
         Version.create(version_name: "Wrath")
-        Version.create(version_name: "SoD")
     end
 end
