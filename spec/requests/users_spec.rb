7555ad37ce249746c2ee08620ddc51bda02557fc
Nicole Beguesse <nicole@monroneylabels.com>
2024-03-05 14:52:20 -0500

    Breaking test for user creation

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,6 +3,15 @@ require 'rake'
 
 describe UsersController, :type=>:request do
 
+    it "creates a user without a username" do
+        expect{
+            @user = User.create({:wow_id=>"867374154", :battletag=>"NiceBest#1557", :uuid=>"ac493fb6-5d4b-4874-b707-82f14b625568"})
+        }.to change(User, :count).by(1)
+        expect{
+           @user = User.create({:wow_id=>"867374454", :battletag=>"NiceBest#3337", :uuid=>"ac493fb6-5d4b-4874-b707-82f1423jk25568"})
+        }.to change(User, :count).by(1)
+    end
+
     it "updates username" do
         @user = FactoryBot.create(:user, {:email=>"test@test.com", :password=>"123456", :password_confirmation=>"123456", :username=>"Bob"})
         post "/api/users/sign_in", {:params=>{:user=>{:email=>"test@test.com", :password=>"123456"}}}
