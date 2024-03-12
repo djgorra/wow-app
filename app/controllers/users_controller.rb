9bf64e823f4eb9bacdd775abe21ac4604930860d
Don Gorra <djgorra93@gmail.com>
2024-03-12 12:19:10 -0400

    Delete user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,10 @@ class UsersController < ApplicationController
     end
 
     def destroy
+        current_user.drops.destroy_all
+        current_user.character.destroy_all
         current_user.destroy
+
         render json: { message: "User deleted" }, status: :ok
     end
 
