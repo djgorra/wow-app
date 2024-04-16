e49bca9621ce7f763bda1552a80d375fc1ffe6ef
Don Gorra <djgorra93@gmail.com>
2024-04-16 13:41:01 -0400

    Team and user admin area updates

--- app/admin/teams.rb
+++ app/admin/teams.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register Team do
-
+    index do 
+        column :id
+        column :user
+        column :name
+        column :characters
+        column :faction
+        column :runs
+        column :created_at
+        actions
+    end
 end
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,10 +1,13 @@
 ActiveAdmin.register User do
-    show do 
-        attributes_table do
-            row :email
-            row :characters
-            row :created_at
-            row :updated_at
-        end
+    index do 
+        column :id
+        column :email
+        column :battletag
+        column :characters
+        column :teams
+        column :runs
+        column :created_at
+        column :updated_at
+        actions
     end
 end
